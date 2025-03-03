e5536cb0a74a845bfcca6287e27ecca4826fcf8b
pawelsawczenko <pawelsawczenko@gmail.com>
2025-03-03 19:56:57 +0300

    add h/a

--- src/main.js
+++ src/main.js
@@ -1,24 +1,69 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// -- H / A - L2 - simple calclator
+//
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+function add(num1, num2) {
+  if (arguments.length < 2) {
+    return function (num) {
+      return num1 + num;
+    };
+  } else {
+    return num1 + num2;
+  }
+}
 
-setupCounter(document.querySelector('#counter'))
+console.log(add(1, 2)); // 3
+console.log(add(1)(2)); // 3
+
+//
+// -- H / A - L2 - 1
+//
+
+Array.prototype.myFilter = function (callbackFn) {
+  const filteredArr = [];
+
+  for (let i = 0; i < this.length - 1; i++) {
+    if (callbackFn(this[i], i, this)) {
+      filteredArr.push(this[i]);
+    }
+  }
+
+  return filteredArr;
+};
+
+const unfilteredArr = [3, 4, 5, 1, 2, 0];
+
+const filteredArr1 = unfilteredArr.myFilter((num) => num >= 3);
+const filteredArr2 = unfilteredArr.myFilter((num) => num < 3);
+
+console.log(filteredArr1); // [3,4,5]
+console.log(filteredArr2); // [1,2]
+
+//
+// -- H / A - L2 - 2
+//
+
+// for testing
+window.alert("alert as alert ");
+window.confirm("confirm as confirm");
+window.prompt("prompt as prompt");
+
+const [alertFn, confirmFn, promptFn] = [
+  window.alert,
+  window.confirm,
+  window.prompt,
+];
+
+window.alert = function (msg) {
+  confirmFn(msg);
+};
+window.confirm = function (msg) {
+  promptFn(msg);
+};
+window.prompt = function (msg) {
+  alertFn(msg);
+};
+
+// for testing
+window.alert("alert as confirm");
+window.confirm("confirm as prompt");
+window.prompt("prompt as alert");
